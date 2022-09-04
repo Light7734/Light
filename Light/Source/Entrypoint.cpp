4d72542ce187951175255ffcf96b60e0a8480fce
Light7734 <mail@mohammad-hm.com>
2022-09-04 23:04:30 +0430

    feat: add 'Light' as the main executable

---
+++ Light/Source/Entrypoint.cpp
@@ -0,0 +1,94 @@
+#include "Logger.hpp"
+#include "Mojula.hpp"
+
+#include <unordered_map>
+#include <vector>
+
+#define CREATE_MODULE(ModuleName)                                  \
+	{                                                              \
+		using namespace Light;                                     \
+		Mojula::Module* module = new ModuleName::ModuleName();     \
+		modules.push_back(module);                                 \
+		modulesData[module->GetUUID()] = module->GetDataPointer(); \
+	}
+
+int main()
+{
+	using namespace Light;
+
+	try
+	{
+		std::vector<Mojula::Module*> modules;
+		std::vector<Mojula::Module*> tickableModules;
+		std::unordered_map<uint64_t, Mojula::ModuleData*> modulesData;
+
+		////////////////////////////////////////////////////////////////
+		/// Create the modules and set up pointers to dependencies
+		{
+			CREATE_MODULE(Logger);
+		}
+
+		for (auto* module : modules)
+		{
+			if (module->IsTickable())
+			{
+				tickableModules.push_back(module);
+			}
+
+			for (uint64_t dependencyUUID : module->GetDependenciesUUID())
+			{
+				module->StoreDependencyDataPointer(dependencyUUID, modulesData[dependencyUUID]);
+			}
+		}
+
+		////////////////////////////////////////////////////////////////
+		/// Enter the game loop
+		{
+			for (auto* module : modules)
+			{
+				module->OnBirth();
+			}
+
+			while (true)
+			{
+				for (auto* module : tickableModules)
+				{
+					module->Tick();
+				}
+			}
+		}
+
+		////////////////////////////////////////////////////////////////
+		/// Gracefully destruct everything
+		{
+			for (auto* module : modules) // do it in reverse order...
+			{
+				module->OnDeath();
+			}
+
+			for (auto* module : modules) // do it in reverse order...
+			{
+				delete module;
+			}
+		}
+	}
+	catch (std::exception& exception)
+	{
+		////////////////////////////////////////////////////////////////
+		/// Save user progress if applicable
+		{
+			//     ???
+		}
+		std::cout << "Excepton thrown m8! --> " << exception.what() << '\n';
+
+		////////////////////////////////////////////////////////////////
+		/// Show/save information about what went wrong, where and when
+		{
+			//     ???
+		}
+
+		// return exception.code
+	}
+
+	return 0;
+}
