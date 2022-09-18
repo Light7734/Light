372002febfbe6f4f691cb3f4a2b68786632bb682
Light7734 <mail@mohammad-hm.com>
2022-09-18 15:03:25 +0430

    feat: event's notif binding macros

--- Modules/Events/Source/Events.hpp
+++ Modules/Events/Source/Events.hpp
@@ -2,6 +2,90 @@
 
 #include <functional>
 
+#define NOTIF_BINDING(name, var, num_args, ...)        NOTIF_BINDING##num_args(name, var, __VA_ARGS__)
+#define STATIC_NOTIF_BINDING(name, var, num_args, ...) STATIC_NOTIF_BINDING##num_args(name, var, __VA_ARGS__)
+
+#define NOTIF_BINDING0(name, var, ...)                                 \
+	template<typename T>                                               \
+	inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance) \
+	{                                                                  \
+		var.AddListener(std::bind(func, instance));                    \
+	}
+
+
+#define NOTIF_BINDING1(name, var, ...)                                     \
+	template<typename T>                                                   \
+	inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance)     \
+	{                                                                      \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1)); \
+	}
+
+
+#define NOTIF_BINDING2(name, var, ...)                                                            \
+	template<typename T>                                                                          \
+	inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance)                            \
+	{                                                                                             \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1, std::placeholders::_2)); \
+	}
+
+
+#define NOTIF_BINDING3(name, var, ...)                                                                                   \
+	template<typename T>                                                                                                 \
+	inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance)                                                   \
+	{                                                                                                                    \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)); \
+	}
+
+
+#define NOTIF_BINDING4(name, var, ...)                                                                                                          \
+	template<typename T>                                                                                                                        \
+	inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance)                                                                          \
+	{                                                                                                                                           \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4)); \
+	}
+
+#define STATIC_NOTIF_BINDING0(name, var, ...)                                 \
+	template<typename T>                                                      \
+	static inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance) \
+	{                                                                         \
+		var.AddListener(std::bind(func, instance));                           \
+	}
+
+#define STATIC_NOTIF_BINDING1(name, var, ...)                                 \
+	template<typename T>                                                      \
+	static inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance) \
+	{                                                                         \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1));    \
+	}                                                                         \
+	static inline void BindStatic_##name(bool (*func)(__VA_ARGS__))           \
+	{                                                                         \
+		var.AddListener(std::bind(func, std::placeholders::_1));              \
+	}
+
+
+#define STATIC_NOTIF_BINDING2(name, var, ...)                                                     \
+	template<typename T>                                                                          \
+	static inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance)                     \
+	{                                                                                             \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1, std::placeholders::_2)); \
+	}
+
+
+#define STATIC_NOTIF_BINDING3(name, var, ...)                                                                            \
+	template<typename T>                                                                                                 \
+	static inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance)                                            \
+	{                                                                                                                    \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)); \
+	}
+
+
+#define STATIC_NOTIF_BINDING4(name, var, ...)                                                                                                   \
+	template<typename T>                                                                                                                        \
+	static inline void Bind_##name(bool (T::*func)(__VA_ARGS__), T* instance)                                                                   \
+	{                                                                                                                                           \
+		var.AddListener(std::bind(func, instance, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3, std::placeholders::_4)); \
+	}
+
 namespace Light {
 
 	/** @brief Notifies the listeners upon invoke */
